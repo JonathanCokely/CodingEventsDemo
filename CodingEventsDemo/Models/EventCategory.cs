178927ec2e35ef64c045e8b9a343110019b20c15
Cokely <JC032926@cerner.net>
2020-12-08 07:32:11 -0600

    Completed exercise 19.4

---
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public class EventCategory
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+        
+        public EventCategory()
+        {    
+        }
+
+        public EventCategory(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EventCategory category &&
+                   Name == category.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
